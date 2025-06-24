958fb60afcb6c0abf17715fa991e5d48b8228f86
藤田大輔 <fujita.daisuke@icloud.com>
2025-06-24 11:33:30 +0900

    test.javaをコミット

---
+++ src/test.java
@@ -0,0 +1,9 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class test {
+    public static void main(String[] args) {
+        String i = "Hello World!";
+            System.out.println(i);
+            System.out.println(i);
+    }
+}
