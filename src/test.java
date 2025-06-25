4709c55a21cb2090d6e87af76ecc0e377f9b525b
fujisuk <fujita.daisuke@icloud.com>
2025-06-25 09:51:45 +0900

    修正（test.java）
    println → printf

--- src/test.java
+++ src/test.java
@@ -3,8 +3,8 @@ import org.w3c.dom.ls.LSOutput;
 public class test {
     public static void main(String[] args) {
         String i = "Hello World!";
-            System.out.println(i);
-            System.out.println(i);
+            System.out.printf(i);
+            System.out.printf(i);
             // コメント
             System.out.println("Hello Hello");
     }
